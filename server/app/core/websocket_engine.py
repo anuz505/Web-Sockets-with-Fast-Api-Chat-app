72bca46b29b16da1ce15cafb1ac1f9ac3665accf
anuz505 <anujb364@gmail.com>
2025-11-26 21:29:37 +0545

    friends ui

--- server/app/core/websocket_engine.py
+++ server/app/core/websocket_engine.py
@@ -38,46 +38,9 @@ class ConnectionManager:
                 await self.disconnect(reciever_id)
                 return False
 
-        # if msg.reciever_id in self.active_connections:
-        #     websocket = self.active_connections[msg.reciever_id]
-        #     await websocket.send_json(
-        #         {"type": "message", "data": msg.model_dump(mode="json")}
-        #     )
-        #     return True
-        # return False
-
     def is_user_online(self, user_id: int) -> bool:
         """Check if user is currently connected"""
         return user_id in self.active_connections
-
-    # async def send_json_message(self, data: dict, user_id: int):
-    #     if user_id in self.active_connections:
-    #         websocket = self.active_connections[user_id]
-    #         await websocket.send_json(data)
-
-    # async def broadcast_status(self, user_id: int, status: str):
-    #     if user_id in self.user_friends:
-    #         for friend_id in self.user_friends[user_id]:
-    #             if friend_id in self.active_connections:
-    #                 await self.send_json_message(
-    #                     {
-    #                         "type": "status_update",
-    #                         "user_id": user_id,
-    #                         "status": status,
-    #                         "timestamp": datetime.now(),
-    #                     },
-    #                     friend_id,
-    #                 )
-
-    # def get_online_friends(self, user_id: int) -> Set[int]:
-    #     """Get list of online friends for a user"""
-    #     if user_id not in self.user_friends:
-    #         return set()
-    #     return {
-    #         friend_id
-    #         for friend_id in self.user_friends[user_id]
-    #         if friend_id in self.active_connections
-    #     }
 
 
 manager = ConnectionManager()
